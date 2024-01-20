d4a646497c0a847421f7ef111f061e8acbfffe3f
nibraaska <nnibraas@gmail.com>
2024-01-20 13:21:55 -0600

    Axios command is working

--- src/service/recipeSearch.tsx
+++ src/service/recipeSearch.tsx
@@ -5,19 +5,22 @@ const searchRecipeAPI = {
 
     fetchData: async () => {
         try {
-            axios.get('https://api.edamam.com/api/recipes/v2', {
-                headers: {
+            axios({
+                method: 'get',
+                url: `https://api.edamam.com/api/recipes/v2`,
+                withCredentials: false,
+                params: {
+                    "Content-Type": "application/x-www-form-urlencoded",
                     "type": "public",
                     "app_id": "e15dfdd0",
                     "app_key": "37050fa2a6145e6dcce970921a7f40e7" ,
                     "q": "chicken",
-                }
-                })
-                .then(response => {
-                console.log(response.data);
+                },
+              }).then(response => {
+                    console.log(response.data);
                 })
                 .catch(error => {
-                console.error('There was an error!', error);
+                    console.error('There was an error!', error);
                 });
         } catch (error) {
           console.error(error);
